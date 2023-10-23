b38fb47e36d9400b2231b3e32cb6dd77ac235d17
Jonathas Quintao <jonathasquintaosilva2002@gmail.com>
2023-10-23 16:28:42 -0300

    So a nabar na tela de cadastro

--- src/views/CadastroFuncionario.js
+++ src/views/CadastroFuncionario.js
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
 import { TextField } from "@mui/material";
 import {Stack} from "@mui/material";
-import navBar from "../components/navbar";
+import NavBar from "../components/navbar";
+import "./inicial.css";
 
 const CadastroFuncionario = () => {
     const [nome, setNome] = useState('')
@@ -14,49 +15,53 @@ const CadastroFuncionario = () => {
 
 
   return (
-    <div className="container"> 
-      <div className="rows">
-        <div className="col-lg-12">
-          <div className="bs-component">
-            <Stack direction='column'>
+    <div className="teste">
+      <NavBar/>
 
-            <TextField
-              label="Nome: *"
-              name="nome"
-              value={nome}
-              onChange={(e) => setNome(e.target.value)}
-            />
-            <TextField
-              label="CPF: *"
-              name="cpf"
-              value={cpf}
-              onChange={(e) => setCpf(e.target.value)}
-              maxLength="11"
-            />
-            <TextField
-              label="Email: *"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-              label="Telefone:"
-              name="telefone"
-              value={telefone}
-              onChange={(e) => setTelefone(e.target.value)}
-            />
-            <TextField
-              label="Endereco:"
-              name="endereco"
-              value={endereco}
-              onChange={(e) => setEndereco(e.target.value)}
-            />
-            </Stack>
-            <Stack direction="row" spacing={1} padding={1}>
+      <div className="container"> 
+        <div className="rows">
+          <div className="col-lg-12">
+            <div className="bs-component">
+              <Stack direction='column'>
 
-            <button type="button" className="btn btn-success">SALVAR</button>
-            <button type="button" className="btn btn-danger">CANCELAR</button>
-            </Stack>
+              <TextField
+                label="Nome: *"
+                name="nome"
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+              />
+              <TextField
+                label="CPF: *"
+                name="cpf"
+                value={cpf}
+                onChange={(e) => setCpf(e.target.value)}
+                maxLength="11"
+              />
+              <TextField
+                label="Email: *"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                label="Telefone:"
+                name="telefone"
+                value={telefone}
+                onChange={(e) => setTelefone(e.target.value)}
+              />
+              <TextField
+                label="Endereco:"
+                name="endereco"
+                value={endereco}
+                onChange={(e) => setEndereco(e.target.value)}
+              />
+              </Stack>
+              <Stack direction="row" spacing={1} padding={1}>
+
+              <button type="button" className="btn btn-success">SALVAR</button>
+              <button type="button" className="btn btn-danger">CANCELAR</button>
+              </Stack>
+            </div>
           </div>
         </div>
       </div>
